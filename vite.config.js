05e05ce9528d7375fbca2d7990431d204dd8558e
humphrey.park <humphrey.park@kakaocorp.com>
2025-09-06 13:52:09 +0900

    fix: GitHub Pages 배포 시 경로 설정 개선

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // GitHub Pages 배포 시 경로 설정
 export default defineConfig({
   plugins: [react()],
-  base: '/' // 레포 이름과 동일하게
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/'
 });
